6da41281c39c06aa4889452a08da1c664ab7a4a3
ziyovuddintolipov <ziyovuddin.developer@gmail.com>
2024-08-31 11:32:35 +0500

    media fix bug

--- src/components/TopicCreation.jsx
+++ src/components/TopicCreation.jsx
@@ -21,8 +21,8 @@ const TopicCreation = () => {
     };
 
     return (
-        <div className="bg-surface-light dark:bg-surface-dark p-6 rounded-lg shadow-lg">
-            <h2 className="text-xl font-semibold mb-4 flex items-center text-primary-light dark:text-primary-dark">
+        <div className="bg-surface-light dark:bg-surface-dark p-4 pb-10 sm:p-6 rounded-lg shadow-lg max-w-full">
+            <h2 className="text-lg sm:text-xl font-semibold mb-4 flex items-center text-primary-light dark:text-primary-dark">
                 <MdTopic className="mr-2" />
                 Mavzu yaratish
             </h2>
@@ -31,7 +31,7 @@ const TopicCreation = () => {
                 value={topicTitle} 
                 onChange={(e) => setTopicTitle(e.target.value)} 
                 placeholder="Mavzu nomi"
-                className="w-full mb-2 p-2 border rounded bg-background-light dark:bg-background-dark text-text-light dark:text-text-dark"
+                className="w-full mb-2 p-2 border rounded bg-background-light dark:bg-background-dark text-text-light dark:text-text-dark text-sm sm:text-base"
             />
             <div className="mb-2 flex items-center">
                 <MdAttachFile className="mr-2 text-primary-light dark:text-primary-dark" />
@@ -39,23 +39,23 @@ const TopicCreation = () => {
                     type="file" 
                     onChange={handlePdfChange} 
                     accept=".pdf"
-                    className="p-2 border rounded flex-grow bg-background-light dark:bg-background-dark text-text-light dark:text-text-dark"
+                    className="p-2 border rounded flex-grow bg-background-light dark:bg-background-dark text-text-light dark:text-text-dark text-sm sm:text-base"
                 />
             </div>
             <textarea 
                 value={practicalTask} 
                 onChange={(e) => setPracticalTask(e.target.value)} 
                 placeholder="Amaliy vazifa"
-                className="w-full mb-2 p-2 border rounded h-32 bg-background-light dark:bg-background-dark text-text-light dark:text-text-dark"
+                className="w-full mb-2 p-2 border rounded h-24 sm:h-32 bg-background-light dark:bg-background-dark text-text-light dark:text-text-dark text-sm sm:text-base"
             />
             <textarea 
                 value={test} 
                 onChange={(e) => setTest(e.target.value)} 
                 placeholder="Test savollari"
-                className="w-full mb-2 p-2 border rounded h-32 bg-background-light dark:bg-background-dark text-text-light dark:text-text-dark"
+                className="w-full mb-2 p-2 border rounded h-24 sm:h-32 bg-background-light dark:bg-background-dark text-text-light dark:text-text-dark text-sm sm:text-base"
             />
             <button 
-                className="mt-4 px-4 py-2 bg-primary-light hover:bg-secondary-light text-text-light rounded-md transition duration-300 ease-in-out dark:bg-primary-dark dark:hover:bg-secondary-dark dark:text-text-dark"
+                className="mt-4 px-3 py-1 sm:px-4 sm:py-2 bg-primary-light hover:bg-secondary-light text-text-light rounded-md transition duration-300 ease-in-out dark:bg-primary-dark dark:hover:bg-secondary-dark dark:text-text-dark text-sm sm:text-base"
                 onClick={createTopic}
             >
                 Mavzuni yaratish
