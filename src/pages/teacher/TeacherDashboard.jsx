96562290529314d975754fea68258e263f2892b5
ziyovuddintolipov <ziyovuddin.developer@gmail.com>
2024-09-09 20:41:47 +0100

    fix

--- src/pages/teacher/TeacherDashboard.jsx
+++ src/pages/teacher/TeacherDashboard.jsx
@@ -3,10 +3,6 @@ import { Outlet } from 'react-router-dom';
 import DashboardLayout from '@/layouts/common/DashboardLayout';
 
 const TeacherDashboard = ({ children }) => {
-    
-
-
-
     return (
         <DashboardLayout>
                 <h1 className="text-2xl font-bold mb-4 text-text-light dark:text-text-dark">O&apos;qituvchi boshqaruv paneli</h1>
