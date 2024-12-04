1142fbd63c15cc5ec1f83ffb2b4f644b3d54c837
Ziyovuddin <86546933+ZiyovuddinTolipov@users.noreply.github.com>
2024-12-04 18:18:54 +0500

    check id

--- src/layouts/common/VideoLessonsDashboard.jsx
+++ src/layouts/common/VideoLessonsDashboard.jsx
@@ -1,7 +1,14 @@
+import { useParams } from 'react-router-dom'
 import Navbar from '../../components/common/Navbar'
 import VideoLessonsSidebar from  '../../components/common/VideoLessonsSidebar'
+import { useEffect } from 'react'
 
 const VideoLessonsDashboard = () => {
+    const {id} = useParams();
+    
+    // useEffect(() =>{
+    //     console.log(id)
+    // },[id])
     return (
         <div className='bg-white text-black dark:bg-black dark:text-white min-h-[100svh] flex'>
             <div className='w-full max-w-[300px] py-4'>
@@ -9,6 +16,7 @@ const VideoLessonsDashboard = () => {
             </div>
             <div className='relative'>
                 <Navbar maxWidth={300} />
+                <h2 className='text-xl text-white mt-20 ml-10'>{id ? "yes" :"no"} sdfdf</h2>
             </div>
         </div>
     )
